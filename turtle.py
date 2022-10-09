7b0515b3d6ad04d7dc6e056dca7089acfde6d903
haneul1111 <112837377+haneul1111@users.noreply.github.com>
2022-10-09 15:21:32 +0900

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,35 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+t.color("blue");
+t.pensize(10);
+t.up();
+t.goto(-150, 0);
+t.down();
+t.circle(80);
+t.color("yellow");
+t.pensize(10);
+t.up();
+t.goto(-20, 0);
+t.down();
+t.circle(80);
+t.color("black");
+t.pensize(10);
+t.up();
+t.goto(110, 0);
+t.down();
+t.circle(80);
+t.color("green");
+t.pensize(10);
+t.up();
+t.goto(-80, -110);
+t.down();
+t.circle(80)
+t.color("red");
+t.pensize(10);
+t.up();
+t.goto(50, -110);
+t.down();
+t.circle(80);
+t._screen.exitonclick();
